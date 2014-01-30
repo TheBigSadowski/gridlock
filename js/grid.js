d6a779bc802eae1ecaf3f42088e2a94bc6a45271
Joe Sadowski <joe.sadowski@gmail.com>
2014-01-30 11:43:10 -0800

    removes some junk

--- js/grid.js
+++ js/grid.js
@@ -21,27 +21,3 @@ module.exports = function(left, right, bottom, top) {
         }
     };
 };
-
-        // public Grid(int left, int right, int bottom, int top)
-        // {
-        //     _left = left;
-        //     _right = right;
-        //     _bottom = bottom;
-        //     _top = top;
-        // }
-
-        // public int CountBlocksSet(int x1, int x2, int y1, int y2)
-        // {
-        //     ++TotalCalls;
-        //     int intersectRight = Math.Min(x2, _right);
-        //     int intersectLeft = Math.Max(x1, _left);
-        //     int intersectTop = Math.Min(y2, _top);
-        //     int intersectBottom = Math.Max(y1, _bottom);
-
-        //     if (intersectRight < intersectLeft)
-        //         return 0;
-        //     if (intersectTop < intersectBottom)
-        //         return 0;
-
-        //     return (intersectRight - intersectLeft + 1) * (intersectTop - intersectBottom + 1);
-        // }
